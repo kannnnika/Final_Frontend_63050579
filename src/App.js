26b6577c55a12a6bf310e727873013b78151729b
kannnnika <63050579@go.buu.ac.th>
2023-10-30 11:15:35 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Container } from "@mui/material";
+import React from "react";
+import Header from "./components/Header";
+import List from "./components/List";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box position="relative">
+      {/* <Header /> */}
+      <Container>
+        <List />
+      </Container>
+    </Box>
   );
-}
+};
 
 export default App;
